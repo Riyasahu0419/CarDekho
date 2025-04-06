c87f222673585be22503e6faf4feaff73b607690
Riya Sahu <141046650+Riyasahu0419@users.noreply.github.com>
2025-04-06 14:55:29 +0530

    server

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ server.use((req, res, next) => {
       console.log('Request from origin:', origin); 
       const allowedOrigins = [
         "http://localhost:5173", 
-        "https://sugar-project.vercel.app", 
+        "https://car-dekho-esrf.vercel.app", 
       ];
       if (!origin || allowedOrigins.includes(origin)) {
         callback(null, true);
